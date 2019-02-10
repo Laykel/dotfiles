e75cda325a32411338dbe75cdba2ab895f3b1a04
Laykel <luc.wachter@gmail.com>
2019-02-10 19:19:37 +0100

    Lots

--- scripts/updateDot.sh
+++ scripts/updateDot.sh
@@ -19,13 +19,20 @@ egrep -v '^#|^ *$' /etc/apt/sources.list /etc/apt/sources.list.d/* > ppasList.tx
 # Update snap packages
 sudo snap refresh
 
-# Update python packages
-
 # Update tmux plugins
 "$HOME/.tmux/plugins/tpm/bin/update_plugins" all
 
 # Update vim plugins
 vim +PlugUpdate +qa
+
+# Update R packages
+sudo R -e 'update.packages()'
+
+# Update Pip packages ?
+# import pip
+# from subprocess import call
+# packages = [dist.project_name for dist in pip.get_installed_distributions()]
+# call("pip install --upgrade " + ' '.join(packages), shell=True)
 
 # Update npm packages
 # npm update -g
